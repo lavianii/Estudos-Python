266b10fa05f032efe78d85470a2817c943acb617
Jonas S. Laviani <89589588+lavianii@users.noreply.github.com>
2022-05-03 13:18:15 -0300

    Dia 03 de Maio 2022

---
+++ InicioAulasPython/ex006.py
@@ -0,0 +1,2 @@
+n = int(input('Digite um valor: '))
+print('O dobro é {}, o triplo e {} a sua raiz quadrada {:0.3}'.format(n * 2, n * 3, n ** (1/2)))
---
+++ InicioAulasPython/ex007.py
@@ -0,0 +1,6 @@
+nota1 = float(input('Digite sua primeira nota: '))
+nota2 = float(input('Digite sua segunda nota: '))
+media = (nota1 + nota2)/2
+
+print('Sua média é: {:.1f}'.format(media))
+
