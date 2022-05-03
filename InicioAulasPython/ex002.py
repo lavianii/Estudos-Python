e552aca6276a840c3c0f43fcd25346f33864e3b4
Jonas S. Laviani <89589588+lavianii@users.noreply.github.com>
2022-05-03 13:17:12 -0300

    Dia 30 de Abril 2022

---
+++ InicioAulasPython/ex002.py
@@ -0,0 +1,4 @@
+nome = input('Digite seu nome: ')
+print('Olá, é um prazer te conhecer,',nome,'!')
+nome2 = input('Digite seu nome novamente: ')
+print('Olá, é um prazer te conhecer, Jon{}!'.format(nome2))
