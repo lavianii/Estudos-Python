e45ed279db4c4effacd2c361bc5ec9385e72bf95
Jonas S. Laviani <89589588+lavianii@users.noreply.github.com>
2022-05-05 13:42:33 -0300

    Dia 05 de Maio 2022

--- Condicionais Python/ex029.py
+++ Condicionais Python/ex029.py
@@ -1,4 +1,4 @@
-km = int(input('Qual a velocidade você passou na pista: '))
+km = int(input('Qual a velocidade que você passou na pista: '))
 multa = (km - 80) * 7
 if km > 80:
     print('Você foi multado, pague R${:.2f}'. format(multa))
