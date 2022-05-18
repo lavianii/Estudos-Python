1fc1a02e1c8743b63c7a7ca9244df7e14a772a54
Jonas S. Laviani <89589588+lavianii@users.noreply.github.com>
2022-05-18 14:46:33 -0300

    Dia 18 de Maio 2022

---
+++ Condicionais Python/ex049.py
@@ -0,0 +1,9 @@
+tabuada = int(input('Digite qual tabuada deseja multipicar: '))
+for t in range(0,11):
+    print('{} X  {} = {}'.format(tabuada, t, tabuada*t))
+
+## Usuário define até onde quer multiplicar
+tabu = int(input('Digite a tabuada: '))
+final = int(input('Digite até onde quer multiplicar: '))
+for i in range(0,final+1):
+    print('{} X {} = {}'.format(tabu, i, tabu*i))
